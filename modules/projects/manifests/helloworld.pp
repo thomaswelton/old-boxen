e790f8348fd55bb3aba6bd1efa304c93bde10e7e
Thomas Welton <thomaswelton@me.com>
2013-10-19 22:16:06 +0100

    refacoring to use a variable for project name

--- modules/projects/manifests/helloworld.pp
+++ modules/projects/manifests/helloworld.pp
@@ -1,56 +1,58 @@
 class projects::helloworld {
 
+	$project_name = 'helloworld'
+
 	# nginx config
-	php::project { 'helloworld':
-		source        => 'helloworldlondon/helloworld',
-		dir 		  => "/Users/${::boxen_user}/Sites/helloworld",
-		mysql         => 'helloworld',
+	php::project { $project_name:
+		source        => 'git@github.com:helloworldlondon/huurdit.git',
+		dir 		  => "/Users/${::boxen_user}/Sites/${project_name}",
+		mysql         => $project_name,
 		nginx         => "${::boxen_repodir}/modules/projects/templates/shared/nginx.laravel.conf.erb",
 		redis         => true,
 		php           => '5.4.17',
 	}
 
-    exec { 'run helloworld composer':
+    exec { "run ${project_name} composer":
 		command => '/opt/boxen/phpenv/bin/composer install',
-		cwd => "/Users/${::boxen_user}/Sites/helloworld",
-		creates => "/Users/${::boxen_user}/Sites/helloworld/vendor",
-		require => Php::Project[helloworld],
+		cwd => "/Users/${::boxen_user}/Sites/${project_name}",
+		creates => "/Users/${::boxen_user}/Sites/${project_name}/vendor",
+		require => Php::Project[$project_name],
 	}
 
-	exec { "db install helloworld":
+	exec { "db install ${project_name}":
 		command => "/opt/boxen/phpenv/shims/php artisan migrate --env=local && /opt/boxen/phpenv/shims/php artisan db:seed --env=local",
-		cwd => "/Users/${::boxen_user}/Sites/helloworld",
-		require => Exec['run helloworld composer'],
-		unless  => "mysql -uroot -p13306 -e 'show tables from helloworld' --password='' | grep migrations"
+		cwd => "/Users/${::boxen_user}/Sites/${project_name}",
+		require => Exec["run ${project_name} composer"],
+		unless  => "mysql -uroot -p13306 -e 'show tables from ${project_name}' --password='' | grep migrations"
     }
 
     # Add heroku git remotes
-    exec { 'add helloworld heroku-dev':
+    exec { "add ${project_name} heroku-dev":
     	command => 'git remote add dev git@heroku.com:helloworldlondon-dev.git',
-    	cwd => "/Users/${::boxen_user}/Sites/helloworld",
+    	cwd => "/Users/${::boxen_user}/Sites/${project_name}",
     	unless => 'git remote | grep dev',
-    	require => Php::Project[helloworld],
+    	require => Php::Project[$project_name],
     }
 
-    exec { 'add helloworld heroku-staging':
+    exec { "add ${project_name} heroku-staging":
     	command => 'git remote add staging git@heroku.com:helloworldlondon-staging.git',
-    	cwd => "/Users/${::boxen_user}/Sites/helloworld",
+    	cwd => "/Users/${::boxen_user}/Sites/${project_name}",
     	unless => 'git remote | grep staging',
-    	require => Php::Project[helloworld],
+    	require => Php::Project[$project_name],
     }
 
-    exec { 'run helloworld npm install':
+    exec { "run ${project_name} npm install":
 		command => '/opt/boxen/nodenv/shims/npm install',
-		cwd => "/Users/${::boxen_user}/Sites/helloworld",
-		creates => "/Users/${::boxen_user}/Sites/helloworld/node_modules",
-		require => Php::Project[helloworld],
+		cwd => "/Users/${::boxen_user}/Sites/${project_name}",
+		creates => "/Users/${::boxen_user}/Sites/${project_name}/node_modules",
+		require => Php::Project[$project_name],
 	}
 
-    exec { 'run helloworld bower install':
+    exec { "run ${project_name} bower install":
 		command => '/opt/boxen/nodenv/shims/bower install',
-		cwd => "/Users/${::boxen_user}/Sites/helloworld",
-		creates => "/Users/${::boxen_user}/Sites/helloworld/bower_components",
-		require => Php::Project[helloworld],
+		cwd => "/Users/${::boxen_user}/Sites/${project_name}",
+		creates => "/Users/${::boxen_user}/Sites/${project_name}/bower_components",
+		require => Php::Project[$project_name],
 	}
 
 }
