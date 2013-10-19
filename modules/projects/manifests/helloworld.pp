2d4c804a1ee6d28dfac733973efbe212e590a119
Thomas Welton <thomaswelton@me.com>
2013-10-19 16:54:49 +0100

    running DB migrations on install and fixed run order of tasks with requires

--- modules/projects/manifests/helloworld.pp
+++ modules/projects/manifests/helloworld.pp
@@ -10,40 +10,47 @@ class projects::helloworld {
 		php           => '5.4.17',
 	}
 
-	# Add heroku git remotes
-	exec { 'add helloworld heroku-dev':
-		command => 'git remote add dev git@heroku.com:helloworldlondon-dev.git',
+    exec { 'run helloworld composer':
+		command => '/opt/boxen/phpenv/bin/composer install',
 		cwd => "/Users/${::boxen_user}/Sites/helloworld",
-		unless => 'git remote | grep dev',
+		creates => "/Users/${::boxen_user}/Sites/helloworld/vendor",
+		require => Php::Project[helloworld],
 	}
 
-	exec { 'add helloworld heroku-staging':
-		command => 'git remote add staging git@heroku.com:helloworldlondon-staging.git',
+	exec { "db install helloworld":
+		command => "/opt/boxen/phpenv/shims/php artisan migrate --env=local && /opt/boxen/phpenv/shims/php artisan db:seed --env=local",
 		cwd => "/Users/${::boxen_user}/Sites/helloworld",
-		unless => 'git remote | grep staging',
+		require => Exec['run helloworld composer'],
+		unless  => "mysql -uroot -p13306 -e 'show tables from helloworld' --password='' | grep migrations"
+    }
+
+    # Add heroku git remotes
+    exec { 'add helloworld heroku-dev':
+    	command => 'git remote add dev git@heroku.com:helloworldlondon-dev.git',
+    	cwd => "/Users/${::boxen_user}/Sites/helloworld",
+    	unless => 'git remote | grep dev',
+    	require => Php::Project[helloworld],
+    }
+
+    exec { 'add helloworld heroku-staging':
+    	command => 'git remote add staging git@heroku.com:helloworldlondon-staging.git',
+    	cwd => "/Users/${::boxen_user}/Sites/helloworld",
+    	unless => 'git remote | grep staging',
+    	require => Php::Project[helloworld],
+    }
+
+    exec { 'run helloworld npm install':
+		command => '/opt/boxen/nodenv/shims/npm install',
+		cwd => "/Users/${::boxen_user}/Sites/helloworld",
+		creates => "/Users/${::boxen_user}/Sites/helloworld/node_modules",
+		require => Php::Project[helloworld],
 	}
 
-	# Run composer on first install if vendor dir not found
-	if ! defined(File["/Users/${::boxen_user}/Sites/helloworld/vendor"]) {
-	    exec { 'run helloworld composer':
-			command => '/opt/boxen/phpenv/bin/composer install',
-			cwd => "/Users/${::boxen_user}/Sites/helloworld"
-		}
+    exec { 'run helloworld bower install':
+		command => '/opt/boxen/nodenv/shims/bower install',
+		cwd => "/Users/${::boxen_user}/Sites/helloworld",
+		creates => "/Users/${::boxen_user}/Sites/helloworld/bower_components",
+		require => Php::Project[helloworld],
 	}
 
-	# Run npm install on first install if node_modules dir not found
-	if ! defined(File["/Users/${::boxen_user}/Sites/helloworld/node_modules"]) {
-	    exec { 'run helloworld npm install':
-			command => '/opt/boxen/nodenv/shims/npm install',
-			cwd => "/Users/${::boxen_user}/Sites/helloworld"
-		}
-	}
-
-	# Run bower install on first install if bower_components dir not found
-	if ! defined(File["/Users/${::boxen_user}/Sites/helloworld/bower_components"]) {
-	    exec { 'run helloworld bower install':
-			command => '/opt/boxen/nodenv/shims/bower install',
-			cwd => "/Users/${::boxen_user}/Sites/helloworld"
-		}
-	}
 }
