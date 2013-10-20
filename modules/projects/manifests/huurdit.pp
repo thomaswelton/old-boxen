ed96d29f8ac3968bcfd999f065e2ae704efcd3ce
thomaswelton <thomaswelton@me.com>
2013-10-20 22:45:07 +0100

    refactoring to use a project_dir variable

--- modules/projects/manifests/huurdit.pp
+++ modules/projects/manifests/huurdit.pp
@@ -3,14 +3,15 @@ class projects::huurdit {
 	include apache
 
 	$project_name = 'huurdit'
+	$project_dir = "/Users/${::boxen_user}/Sites/${project_name}"
 
 	## Create Apache project running on port 10080
 	## Proxy the apache server through nginx on port 80
 	apache::php::project { $project_name:
 		port 		  => 10080,
 		source        => "helloworldlondon/${project_name}",
-		dir 		  => "/Users/${::boxen_user}/Sites/${project_name}",
-		docroot       => "/Users/${::boxen_user}/Sites/${project_name}/public",
+		dir 		  => $project_dir,
+		docroot       => "${project_dir}/public",
 		mysql         => $project_name,
 		redis         => true,
 		php           => '5.4.17',
@@ -19,36 +20,36 @@ class projects::huurdit {
 
     exec { "run ${project_name} composer":
 		command => '/opt/boxen/phpenv/bin/composer install',
-		cwd => "/Users/${::boxen_user}/Sites/${project_name}",
-		creates => "/Users/${::boxen_user}/Sites/${project_name}/vendor",
+		cwd => "${project_dir}",
+		creates => "${project_dir}/vendor",
 		require => Apache::Php::Project[$project_name],
 	}
 
 	exec { "db install ${project_name}":
 		command => "/opt/boxen/phpenv/shims/php artisan migrate --env=local && /opt/boxen/phpenv/shims/php artisan db:seed --env=local",
-		cwd => "/Users/${::boxen_user}/Sites/${project_name}",
+		cwd => "${project_dir}",
 		require => Exec["run ${project_name} composer"],
 		unless  => "mysql -uroot -p13306 -e 'show tables from ${project_name}' --password='' | grep migrations"
     }
 
     exec { "add ${project_name} heroku-staging":
     	command => 'git remote add staging git@heroku.com:${project_name}-staging.git',
-    	cwd => "/Users/${::boxen_user}/Sites/${project_name}",
+    	cwd => "${project_dir}",
     	unless => 'git remote | grep staging',
     	require => Apache::Php::Project[$project_name],
     }
 
     exec { "run ${project_name} npm install":
 		command => '/opt/boxen/nodenv/shims/npm install',
-		cwd => "/Users/${::boxen_user}/Sites/${project_name}",
-		creates => "/Users/${::boxen_user}/Sites/${project_name}/node_modules",
+		cwd => "${project_dir}",
+		creates => "${project_dir}/node_modules",
 		require => Apache::Php::Project[$project_name],
 	}
 
     exec { "run ${project_name} bower install":
 		command => '/opt/boxen/nodenv/shims/bower install',
-		cwd => "/Users/${::boxen_user}/Sites/${project_name}",
-		creates => "/Users/${::boxen_user}/Sites/${project_name}/bower_components",
+		cwd => "${project_dir}",
+		creates => "${project_dir}/bower_components",
 		require => Apache::Php::Project[$project_name],
 	}
 
